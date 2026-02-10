57e0d3b248a1d1e3183a6ebcb76aee4f05d66eba
patranoiu-ship-it <patranoiu.alexandrugabriel.studente@itispaleocapa.it>
2026-02-10 08:58:51 +0100

    Add files via upload

---
+++ esercizio_26.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+//funzioni
+
+void add(int a[],int &dim){
+	//operazioni peer l'inserimento
+	for(int i=0; i<dim; i++)
+	{
+		
+	}
+}
+
+int main(int argc, char** argv) {
+	
+	//menù
+	int arr[100];
+	int n=0;
+	int opzione;
+	do{
+		//visualizzazione voci
+		cout<<"1 - inserimento";
+		//altre voci
+		cout<<"scegli l'opzione";
+		cin>>opzione;
+		switch(opzione){
+			1://operazione 1
+			    
+				add(arr,n);
+				break;
+				
+			2://operazione 2
+				break;	
+				
+		}
+		
+	}while(opzione!=0);
+	
+	return 0;
+}
